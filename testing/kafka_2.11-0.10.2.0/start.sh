234ad40dbc771f1e64333aef55785615f1435e92
Ethan James Wright <ewright362@gmail.com>
2017-03-13 17:59:48 -0600

    Update start.sh

--- testing/kafka_2.11-0.10.2.0/start.sh
+++ testing/kafka_2.11-0.10.2.0/start.sh
@@ -3,6 +3,3 @@ bin/kafka-server-start.sh config/server.properties &
 bin/kafka-server-start.sh config/server-1.properties &
 bin/kafka-server-start.sh config/server-2.properties &
 
-bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic TestTopic --partitions 2 --replication-factor 3 &
-
-bin/kafka-topics.sh --zookeeper localhost:2181 --describe --topic TestTopic
