c1fadd778b14a6c7788ca611a45effde5c9b855b
Ethan Wright <ethanwright@rgnt2-109-88-dhcp.int.colorado.edu>
2017-04-05 18:05:09 -0600

    Changed topic name for parity w/ java

--- testing/kafka_2.11-0.10.2.0/start.sh
+++
@@ -1,5 +0,0 @@
-bin/zookeeper-server-start.sh config/zookeeper.properties &
-bin/kafka-server-start.sh config/server.properties &
-bin/kafka-server-start.sh config/server-1.properties &
-bin/kafka-server-start.sh config/server-2.properties &
-
--- testing/kafka_2.11-0.10.2.0/start_topic.sh
+++ testing/kafka_2.11-0.10.2.0/start_topic.sh
@@ -1,4 +1,4 @@
-bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic TestTopicXYZ --partitions 2 --replication-factor 2
+bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic twitter-topic --partitions 2 --replication-factor 2
 
 
 
