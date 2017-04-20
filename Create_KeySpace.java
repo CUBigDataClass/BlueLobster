1cb36407ba753845f5d5721c18108729aaf9cdd0
Ethan James Wright <ewright362@gmail.com>
2017-04-20 03:49:06 -0600

    Adding test file for Cassandra interaction with java

---
+++ Create_KeySpace.java
@@ -0,0 +1,25 @@
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+public class Create_KeySpace {
+
+   public static void main(String args[]){
+
+      //Query
+      String query = "CREATE KEYSPACE tp WITH replication "
+         + "= {'class':'SimpleStrategy', 'replication_factor':1};";
+
+      //creating Cluster object
+      Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+
+      //Creating Session object
+      Session session = cluster.connect();
+
+      //Executing the query
+      session.execute(query);
+
+      //using the KeySpace
+      session.execute("USE tp");
+      System.out.println("Keyspace created");
+   }
+}
