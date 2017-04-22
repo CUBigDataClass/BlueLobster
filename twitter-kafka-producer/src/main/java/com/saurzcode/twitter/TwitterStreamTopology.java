fc37f1290f3599e259ac74ef305133d3d85f9f66
parthmishra <pmishraworld@mac.com>
2017-04-21 18:21:46 -0600

    adding storm-kafka integration

---
+++ twitter-kafka-producer/src/main/java/com/saurzcode/twitter/TwitterStreamTopology.java
@@ -0,0 +1,23 @@
+package com.saurzcode.twitter;
+
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+
+//import storm configuration packages
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+
+public class TwitterStreamTopology {
+	public static void main(String[] args) throws Exception {
+		Config config = new Config();
+		config.setDebug(true);
+		
+		TopologyBuilder builder = new TopologyBuilder();
+		
+		
+		
+	}
+	
+
+}
