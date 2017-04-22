fab0ff8aebb8bc597356a96d60d9e7285d610504
parthmishra <pmishraworld@mac.com>
2017-04-22 00:33:29 -0600

    Added kafkaSpout and location filtering bolt

--- twitter-kafka-producer/src/main/java/com/saurzcode/twitter/TwitterStreamTopology.java
+++ twitter-kafka-producer/src/main/java/com/saurzcode/twitter/TwitterStreamTopology.java
@@ -1,10 +1,21 @@
 package com.saurzcode.twitter;
 
+import java.util.UUID;
+
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 
 //import storm configuration packages
 import org.apache.storm.Config;
+import org.apache.storm.kafka.KafkaSpout;
+import org.apache.storm.kafka.KafkaConfig;
+import org.apache.storm.kafka.SpoutConfig;
+import org.apache.storm.spout.SchemeAsMultiScheme;
+import org.apache.storm.kafka.StringScheme;
+import org.apache.storm.kafka.BrokerHosts;
+import org.apache.storm.kafka.ZkHosts;
+//import org.apache.storm.kafka.bolt.KafkaBolt; 
+
 import org.apache.storm.LocalCluster;
 import org.apache.storm.topology.TopologyBuilder;
 
@@ -13,7 +24,21 @@ public class TwitterStreamTopology {
 		Config config = new Config();
 		config.setDebug(true);
 		
+		// set kafka properties
+		String topicName = "twitter-topic";
+		String zkConnString = "localhost:9092";
+		
+
+		BrokerHosts hosts = new ZkHosts(zkConnString);
+		SpoutConfig spoutConfig = new SpoutConfig(hosts, topicName, "/" + topicName, UUID.randomUUID().toString());
+		spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+		KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
+		
+		
+		// set topology
 		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("kafkaspout", kafkaSpout);
+		builder.setBolt("geo-filter", new LocationBolt()).shuffleGrouping("kafka-spout");
 		
 		
 		
