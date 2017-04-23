2ac79463560ec441bf4b30fe0e949399c399c685
Max Lookabaugh <malo7591@colorado.edu>
2017-04-23 15:09:28 -0600

    updating for newest version of cassandra

--- scripts/CassandraSetup.sh
+++ scripts/CassandraSetup.sh
@@ -1,7 +1,8 @@
 sudo yum -y update
 sudo yum -y install java
 sudo vi /etc/yum.repos.d/datastax.repo
-sudo yum -y install dsc20
+sudo yum -y install dsc30
+sudo yum install cassandra30-tools
 sudo service cassandra start
 sudo systemctl status cassandra
 sudo systemctl enable cassandra
