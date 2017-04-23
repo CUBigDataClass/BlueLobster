14279efeeebaa63a8c200d8388a82b17df625d56
Ethan Wright <ethanwright@Ethans-MacBook-Air.local>
2017-04-23 15:42:44 -0600

    Cleaned up dependencies

--- cassandra_testing/kafka_to_cassandra.py
+++ cassandra_testing/kafka_to_cassandra.py
@@ -7,18 +7,21 @@ session = cluster.connect('json_data')
 
 consumer = KafkaConsumer('twitter-topic')
 for message in consumer:
-    twitter = json.loads(message.value)
-    twitter_text = twitter["text"]
-    twitter_id = twitter["id"]
-    twitter_id = twitter_id % 100
-    screen_name = twitter["user"]["screen_name"]
-    session.execute(
-            """
-            INSERT INTO tweets (tweets_id, tweets_name, tweets_contents)
-            VALUES (%s, %s, %s)
-            """,
-            (twitter_id, screen_name, twitter_text)
-            )
-    print twitter_text
+    try:
+        twitter = json.loads(message.value)
+        twitter_text = twitter["text"]
+        twitter_id = twitter["id"]
+        twitter_id = twitter_id % 100
+        screen_name = twitter["user"]["screen_name"]
+        session.execute(
+                """
+                INSERT INTO tweets (tweets_id, tweets_name, tweets_contents)
+                VALUES (%s, %s, %s)
+                """,
+                (twitter_id, screen_name, twitter_text)
+                )
+        print twitter_text
+    except KeyError, e:
+        print e
 
 # Doing mod 100 because it isn't an int otherwise
