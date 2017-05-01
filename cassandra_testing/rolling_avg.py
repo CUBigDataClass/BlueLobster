8ef9e41b67ae6100368ea44288aa6d0d1f28b14f
Cloud User <ec2-user@ip-172-31-35-184.us-west-2.compute.internal>
2017-05-01 19:48:55 -0400

    Adding the csvs

--- cassandra_testing/rolling_avg.py
+++ cassandra_testing/rolling_avg.py
@@ -37,8 +37,8 @@ while(True):
 		total_seen[i] = 1
 	    average = states[i]/total_seen[i]
 
-            avg_csv = avg_csv + average + ','
-            states_csv = states_csv + state_list[i] + ','
+            avg_csv = avg_csv + str(average) + ','
+            states_csv = states_csv + str(state_list[i]) + ','
             csv_state = open('state_data.csv', 'w')
             csv_avg = open('avg_data.csv', 'w')
             csv_state.write(states_csv)
