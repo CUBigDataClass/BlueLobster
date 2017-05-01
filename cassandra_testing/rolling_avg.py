00bc373243b49785a6b31f5bf1b9e308678b3017
Cloud User <ec2-user@ip-172-31-35-184.us-west-2.compute.internal>
2017-05-01 18:07:14 -0400

    EVERYTHING WORKS

--- cassandra_testing/rolling_avg.py
+++ cassandra_testing/rolling_avg.py
@@ -1,4 +1,5 @@
 from cassandra.cluster import Cluster
+import time
 state_list = ["Alabama", "Alaska", "Arizona ", "Arkansas ", "California", "Colorado",
                 "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
                 "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
@@ -12,27 +13,32 @@ states = [0] * 50
 total_seen = [0] * 50
 cluster = Cluster()
 session = cluster.connect('storm')
-results = session.execute("SELECT * FROM storm_data")
-for x in results:
-    rows = x
-    state_name = rows[1]
-    state_sentiment = rows[2]
-    for i in range(0, len(state_list)):
-        if(state_list[i] == state_name):
-            states[i] += float(state_sentiment)
-            total_seen[i] += 1.0
+while(True):
+	results = session.execute("SELECT * FROM storm_data")
+	for x in results:
+	    rows = x
+	    state_name = rows[1]
+	    state_name = state_name.lower()
+	    state_sentiment = rows[2]
+	    for i in range(0, len(state_list)):
+		state_list[i] = state_list[i].lower()
+		if(state_list[i] == state_name):
+		    states[i] += float(state_sentiment)
+		    total_seen[i] += 1.0
 
 
-for i in range(0, len(states)):
-    if(total_seen[i] == 0):
-        total_seen[i] = 1
-    average = states[i]/total_seen[i]
-    insert = session.execute(
-            """
-            INSERT INTO website_data (id, state_name, state_sentiment)
-            VALUES(%s, %s, %s)
-            """,
-            (i, state_list[i], average)
-            )
 
-
+	for i in range(0, len(states)):
+	    if(total_seen[i] == 0):
+		total_seen[i] = 1
+	    average = states[i]/total_seen[i]
+	#    print 'states is: ', state_list[i], states[i]
+	#    print 'average is: ', average
+	    insert = session.execute(
+		    """
+		    INSERT INTO website_data (id, state_name, state_sentiment)
+		    VALUES(%s, %s, %s)
+		    """,
+		    (i, state_list[i], average)
+		    )
+        time.sleep(30)
