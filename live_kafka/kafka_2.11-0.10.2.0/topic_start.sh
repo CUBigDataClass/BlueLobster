6f32c2b6e9e72c1ad0bcecc95d7248cbfb1c1a35
Ethan Wright <ethanwright@engr2-13-181-dhcp.int.colorado.edu>
2017-05-01 11:28:05 -0600

    Should work now

--- live_kafka/kafka_2.11-0.10.2.0/topic_start.sh
+++ live_kafka/kafka_2.11-0.10.2.0/topic_start.sh
@@ -1,1 +1,1 @@
-bin/kafka-topics.sh --zookeeper localhost:2181 --alter --topic twitter-topic -access --config retention.ms=18000000
+bin/kafka-topics.sh --zookeeper localhost:2181 --alter --topic twitter-topic -access --config retention.ms=1800000
