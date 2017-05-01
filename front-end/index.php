e2d61f43ee6bc3f53444528da09409f8aacc034b
eml2828 <edmond.lee@colorado.edu>
2017-05-01 12:24:33 -0600

    Add files via upload

--- front-end/index.php
+++ front-end/index.php
@@ -3,52 +3,51 @@
 <head>
 <meta charset="utf-8">
 <title>Twitter Sentiment</title>
-<link rel=stylesheet href="style.css">
+<meta http-equiv="refresh" content="60">
+<link rel=stylesheet href="css/style.css">
 </head>
 <body>
-    <div class = "top">
-        
-    <div class="navlist">
-        <a href="index.html">Home</a>
-        <a href="about.html">About</a>
-        <a>Tech Stack</a>
-    </div>
-    <h1>Twitter Sentiment Analysis</h1>
+  <?php
+
+
+      $cluster   = Cassandra::cluster()
+                     //->withContactPoints('127.0.0.1')
+                     ->build();
+      $session   = $cluster->connect("storm");
+      $statement = new Cassandra\SimpleStatement("SELECT * FROM storm_data");
+      $result    = $session->execute($statement);
+      $session->close();
+      $file = fopen('return.csv', 'w');
+
+      	// save the column headers
+      	fputcsv($file, array('id', 'state', 'sentiment'));
+
+
+      	// save each row of the data
+      	foreach ($result as $row)
+      	{
+      		fputcsv($file, $row);
+      	}
+
+      	// Close the file
+      	fclose($file);
+
+
+    ?>
     <script src="https://d3js.org/d3.v3.min.js"></script>
-    <script src ="map.js" type="text/javascript"> </script>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script type = "text/javascript">
-function myAjax () {
-$.ajax( { type : 'POST',
-          data : { },
-          url  : 'test2.php',              // <=== CALL THE PHP FUNCTION HERE.
-          success: function ( data ) {
-            //alert( data );               // <=== VALUE RETURNED FROM FUNCTION.
-          },
-          error: function ( xhr ) {
-            alert( "error" );
-          }
-        });
-}
-    </script>    
+    <div class = "top">
 
- 
-        
-     <button onclick="myAjax()">Click here</button> <!-- BUTTON CALL PHP FUNCTION -->
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin tincidunt ex, at elementum enim tristique ut. Proin tempor a diam accumsan varius. Duis semper efficitur leo non consectetur. Maecenas tempus eros eu iaculis mattis. Ut eu convallis sapien, vitae imperdiet neque. Suspendisse vitae augue rhoncus, egestas sapien accumsan, vestibulum ligula. Donec cursus porttitor sapien in sodales. Phasellus erat massa, fermentum id sagittis non, finibus at mauris. Aliquam vitae diam quis enim euismod posuere. Etiam nunc turpis, gravida vitae dapibus in, efficitur vel nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam scelerisque leo vitae placerat dignissim. Morbi et enim et nunc elementum sagittis id id sapien. Donec congue ex nec nulla congue iaculis. Quisque id luctus dui. Phasellus ultricies lectus arcu, in lobortis justo laoreet nec.
-
-Phasellus vehicula laoreet porttitor. Nullam congue ligula non posuere feugiat. Curabitur tristique eget tellus maximus eleifend. Nunc sed vulputate enim. In dapibus lorem quis malesuada fringilla. Praesent a sagittis risus, vitae eleifend risus. Donec eget rutrum tortor. Quisque ligula est, ornare non ornare non, tempus vel odio. Fusce non dolor a arcu lacinia volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-
-Quisque eleifend elementum dolor, in mattis neque mollis sed. Aliquam porttitor massa non est hendrerit, vitae commodo quam placerat. Donec tincidunt mi est, id euismod tortor sollicitudin vel. Duis imperdiet, nisi in pellentesque rutrum, nisi elit lacinia arcu, sit amet porta mauris est sit amet ipsum. Curabitur eget rhoncus metus. Nulla facilisi. Vivamus sollicitudin urna velit. Pellentesque finibus vel magna nec euismod. Vivamus finibus eros in purus porttitor, id vestibulum purus vehicula. Fusce non tellus non mauris tempus dapibus. Quisque erat nibh, ultrices vitae urna ac, pharetra porttitor sem. Nunc justo elit, pulvinar nec maximus in, malesuada quis nunc. Nunc condimentum non nulla id blandit. Nam arcu lectus, varius vel ante fringilla, vehicula ultricies nunc. Fusce porta vulputate facilisis.
-
-Sed ullamcorper tortor arcu, vel maximus ipsum aliquet vel. Proin lacinia quam eu sem semper scelerisque. Vestibulum porttitor facilisis mattis. Proin a diam at sapien vulputate eleifend non quis magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum lobortis faucibus. Nulla egestas, ex eu faucibus blandit, tortor justo interdum nibh, ac pretium nunc mi nec metus. Maecenas a libero suscipit, suscipit erat id, rhoncus enim. Sed fermentum mauris dolor, eget blandit dolor rhoncus non. Proin suscipit rutrum quam.
-
-Vestibulum euismod nec nunc et cursus. Praesent vitae suscipit enim, id sagittis lorem. Nunc eget fermentum leo. Nunc non magna bibendum, tincidunt leo vitae, aliquam ligula. Praesent fermentum viverra elit, mollis rutrum erat. Curabitur leo neque, rutrum non magna id, tincidunt aliquam elit. Quisque mi diam, malesuada a tortor at, ultricies venenatis felis. Proin et suscipit lectus. Nam justo turpis, condimentum id mollis ut, elementum vel dui. Vivamus id nibh sapien. Nam porta viverra dui sed porttitor. Nunc fermentum quam dolor, ut accumsan enim laoreet in. Fusce quis iaculis sapien. Suspendisse blandit vestibulum lacus, et cursus justo aliquet at. Fusce lobortis faucibus libero, vitae vulputate ligula condimentum in. Nulla tempor metus sit amet iaculis tempor.
-
-</p>
+    <div class="navlist">
+        <a href = "index.php">Home</a>
+        <a href = "about.html">About</a>
+        <a href = "technologies.html">Technologies</a>
     </div>
-    
-</body>
+      <h1>Twitter Sentiment Analysis</h1>
+      <script src ="js/map.js" type="text/javascript"> </script>
+    </div>
+
+  </body>
     <footer>copyright 2017</footer>
 </html>
-
---
+++ front-end/js/map.js
@@ -0,0 +1,188 @@
+/*  This visualization was made possible by modifying code provided by:
+
+Scott Murray, Choropleth example from "Interactive Data Visualization for the Web"
+https://github.com/alignedleft/d3-book/blob/master/chapter_12/05_choropleth.html
+
+Malcolm Maclean, tooltips example tutorial
+http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
+
+Mike Bostock, Pie Chart Legend
+http://bl.ocks.org/mbostock/3888852  */
+
+
+//Width and height of map
+var width = 960;
+var height = 500;
+
+// D3 Projection
+var projection = d3.geo
+           .albersUsa()
+				   .translate([width/2, height/2])    // translate to center of screen
+				   .scale([1000]);          // scale things down so see entire US
+
+// Define path generator
+var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
+		  	 .projection(projection);  // tell path generator to use albersUsa projection
+
+
+// Define linear scale for output
+var color = d3.scale.linear()
+			  .range(["rgb(0,0,255)","rgb(255,0,0)"]);
+
+//var legendText = ["Cities Lived", "States Lived", "States Visited", "Nada"];
+var w = 140, h = 400;
+
+var key = d3.select("body")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+var legend = key.append("defs")
+          .append("svg:linearGradient")
+          .attr("id", "gradient").attr("x1", "100%")
+          .attr("y1", "0%").attr("x2", "100%")
+          .attr("y2", "100%").attr("spreadMethod", "pad");
+
+			legend.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", "rgb(255,0,0)")
+            .attr("stop-opacity", 1);
+
+			legend.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", "rgb(0,0,255)")
+            .attr("stop-opacity", 1);
+
+			key.append("rect")
+         .attr("width", w - 100)
+         .attr("height", h - 100)
+         .style("fill", "url(#gradient)")
+         .attr("transform", "translate(0,10)");
+
+			var y = d3.scale.linear().range([300, 0]).domain([-5, 5]);
+
+			var yAxis = d3.svg.axis().scale(y).orient("right");
+
+			key.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(41,10)")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 30)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Sentiment Score");
+
+
+
+
+//Create SVG element and append map to the SVG
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height);
+
+// Append Div for tooltip to SVG
+var div = d3.select("body")
+		    .append("div")
+    		.attr("class", "tooltip")
+    		.style("opacity", 0);
+
+// Load in my states data!
+d3.csv("../return.csv", function(data) {
+color.domain([-5,5]); // setting the range of the input data
+
+// Load GeoJSON data and merge with states data
+d3.json("../us-states.json", function(json) {
+
+// Loop through each state data value in the .csv file
+for (var i = 0; i < data.length; i++) {
+
+	// Grab State Name
+	var dataState = data[i].state;
+
+	// Grab data value
+	var dataValue = data[i].sentiment;
+
+	// Find the corresponding state inside the GeoJSON
+	for (var j = 0; j < json.features.length; j++)  {
+		var jsonState = json.features[j].properties.name;
+
+		if (dataState == jsonState) {
+
+		// Copy the data value into the JSON
+		json.features[j].properties.visited = dataValue;
+
+		// Stop looking through the JSON
+		break;
+		}
+	}
+}
+
+// Bind the data to the SVG and create one path per GeoJSON feature
+svg.selectAll("path")
+	.data(json.features)
+	.enter()
+	.append("path")
+	.attr("d", path)
+	.style("stroke", "#fff")
+	.style("stroke-width", "1")
+	.style("fill", function(d) {
+
+	// Get data value
+	var value = d.properties.visited;
+
+	if (value) {
+	//If value exists…
+	return color(value);
+	} else {
+	//If value is undefined…
+	return "rgb(213,222,217)";
+	}
+});
+
+
+// Map the cities I have lived in!
+/*d3.csv("cities-lived.csv", function(data) {
+
+svg.selectAll("circle")
+	.data(data)
+	.enter()
+	.append("circle")
+	.attr("cx", function(d) {
+		return projection([d.lon, d.lat])[0];
+	})
+	.attr("cy", function(d) {
+		return projection([d.lon, d.lat])[1];
+	})
+	.attr("r", function(d) {
+		return Math.sqrt(d.years) * 4;
+	})
+		.style("fill", "rgb(217,91,67)")
+		.style("opacity", 0.85)
+
+	// Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks"
+	// http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
+	.on("mouseover", function(d) {
+    	div.transition()
+      	   .duration(200)
+           .style("opacity", .9);
+           div.text(d.place)
+           .style("left", (d3.event.pageX) + "px")
+           .style("top", (d3.event.pageY - 28) + "px");
+	})
+
+    // fade out tooltip on mouse out
+    .on("mouseout", function(d) {
+        div.transition()
+           .duration(500)
+           .style("opacity", 0);
+    });
+});*/
+
+// Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
+
+	});
+
+});
