b1506db4d45d0cf607659201fde22e9a935e5da3
Ethan Wright <ethanwright@Ethans-MacBook-Air.local>
2017-05-02 20:17:06 -0600

    maybe fixed other_rolling

--- cassandra_testing/other_rolling.py
+++ cassandra_testing/other_rolling.py
@@ -8,50 +8,34 @@ state_list = ["Alabama", "Alaska", "Arizona ", "Arkansas ", "California", "Color
                 "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia",
                 "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
-
 states = [0] * 50
 total_seen = [0] * 50
-
-avg_csv = ''
-states_csv = ''
-
 cluster = Cluster()
 session = cluster.connect('storm')
 while(True):
-	results = session.execute("SELECT * FROM storm_data")
-	for x in results:
-	    rows = x
-	    state_name = rows[1]
-	    state_name = state_name.lower()
-	    state_sentiment = rows[2]
-	    for i in range(0, len(state_list)):
-		state_list[i] = state_list[i].lower()
-		if(state_list[i] == state_name):
-		    states[i] += float(state_sentiment)
-		    total_seen[i] += 1.0
-
-
+    results = session.execute("SELECT * FROM storm_data")
+    for x in results:
+        rows = x
+        state_name = rows[1]
+        state_name = state_name.lower()
+        state_sentiment = rows[2]
+        for i in range(0, len(state_list)):
+            state_list[i] = state_list[i].lower()
+            if(state_list[i] == state_name):
+                states[i] += float(state_sentiment)
+                total_seen[i] += 1.0
 
 	for i in range(0, len(states)):
-	    if(total_seen[i] == 0):
-		total_seen[i] = 1
-	    average = states[i]/total_seen[i]
+            if(total_seen[i] == 0):
+                total_seen[i] = 1
+    	    average = states[i]/total_seen[i]
+            print 'The average is: ', average, ' for state: ', state_list[i]
+    	    insert = session.execute(
+    		    """
+    		    INSERT INTO website_data (id, state_name, state_sentiment)
+    		    VALUES(%s, %s, %s)
+    		    """,
+    		    (i, state_list[i], average)
+    		    )
 
-        avg_csv = avg_csv + average + ','
-        states_csv = states_csv + state_list[i] + ','
-        csv_state = open('state_data.csv', 'w')
-        csv_avg = open('avg_data.csv', 'w')
-        csv_state.write(states_csv)
-        csv_avg.write(avg_csv)
-        csv_state.close()
-        csv_avg.close()
-
-	    insert = session.execute(
-		    """
-		    INSERT INTO website_data (id, state_name, state_sentiment)
-		    VALUES(%s, %s, %s)
-		    """,
-		    (i, state_list[i], average)
-		    )
-
-        time.sleep(30)
+    time.sleep(30)
